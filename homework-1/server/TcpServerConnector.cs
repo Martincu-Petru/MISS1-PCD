12fdd7c9229a9223821a78db87e94504c35b1acb
Petru <martincu.petru@gmail.com>
2020-02-24 21:24:24 +0200

    Fixed push of TcpServerConnector

--- homework-1/server/TcpServerConnector.cs
+++ homework-1/server/TcpServerConnector.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace server
 {
-    class TcpServerConnector
+    internal static class TcpServerConnector
     {
+        private static void Process(object arg)
+        {
+            Console.WriteLine("TCP server thread started");
+
+            try
+            {
+                var server = (TcpListener)arg;
+                var buffer = new byte[65535];
+
+                server.Start();
+
+                for (; ; )
+                {
+                    var client = server.AcceptTcpClient();
+
+                    using (var stream = client.GetStream())
+                    {
+                        int count;
+                        while ((count = stream.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            Console.WriteLine("TCP: " + Encoding.ASCII.GetString(buffer, 0, count));
+                        }
+                    }
+                    client.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                if (ex.ErrorCode != 10004)
+                {
+                    Console.WriteLine("TCPServerProc exception: " + ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("TCPServerProc exception: " + ex);
+            }
+
+            Console.WriteLine("TCP server thread finished");
+        }
     }
 }
