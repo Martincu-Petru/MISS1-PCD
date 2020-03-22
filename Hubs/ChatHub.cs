befea72491765c139a660dc81b0b14b1520e4385
Petru <martincu.petru@gmail.com>
2020-03-22 17:53:11 +0200

    Messages are now back up to database

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,35 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Google.Cloud.Language.V1;
+using LightChatApp.Controllers;
+using LightChatApp.Models;
 
 namespace LightChatApp.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string text)
         {
+            if (user?.Length == 0)
+            {
+                user = "Unknown";
+            }
+
+            if (text?.Length == 0)
+            {
+                text = "Nothing";
+            }
+            MessageController.AddMessage(new Message(user, text));
+
             var client = LanguageServiceClient.Create();
             var response = client.AnalyzeSentiment(new Document()
             {
-                Content = message,
+                Content = text,
                 Type = Document.Types.Type.PlainText
             });
             var sentiment = response.DocumentSentiment;
 
             if (sentiment.Score >= 0.5 && sentiment.Magnitude >= 0.3)
             {
-                message += " 😍";
+                text += " 😍";
             }
             else if (sentiment.Score <= -0.3 && sentiment.Magnitude >= 0.3)
             {
-                message += " 😭";
+                text += " 😭";
             }
             else
             {
-                message += " 🤔";
+                text += " 🤔";
             }
 
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, text);
         }
+
     }
 }
