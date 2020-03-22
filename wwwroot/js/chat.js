f666c46cb2d81389cdb930408a581db4af12034e
Petru <martincu.petru@gmail.com>
2020-03-22 20:57:54 +0200

    A history of previous messages will be displayed on load

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -2,41 +2,67 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
-
 connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    if (user.length === 0) {
-        user = "Unknown";
-    }
-    if (msg.length === 0) {
-        msg = "Nothing";
-    }
-    var encodedUser = user + " - ";
-    var encodedMessage = msg;
-    var b = document.createElement("b");
-    var div = document.createElement("div");
-    b.textContent = encodedUser;
-    div.textContent = encodedMessage;
-    div.classList.add("alert");
-    div.classList.add("alert-success");
-    div.prepend(b);
-    document.getElementById("messagesList").appendChild(div);
+    var encodedUsernameText = user + " - ";
+    var encodedMessageText = message;
+    var usernameText = document.createElement("b");
+    var messageText = document.createElement("div");
+
+    usernameText.textContent = encodedUsernameText;
+    messageText.textContent = encodedMessageText;
+    messageText.classList.add("alert");
+    messageText.classList.add("alert-success");
+    messageText.prepend(usernameText);
+
+    document.getElementById("messagesList").appendChild(messageText);
 
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
+connection.on("ReceiveHistory", function (messages) {
+    messages.forEach(message => {
+        var usernameText = document.createElement("b");
+        var messageText = document.createElement("div");
+        console.log(message);
+        usernameText.textContent = message.user + " - ";
+        messageText.textContent = message.text;
+        messageText.classList.add("alert");
+        messageText.classList.add("alert-success");
+        messageText.prepend(usernameText);
+
+        document.getElementById("messagesList").appendChild(messageText);
+    });
 });
+
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    var user = document.getElementById("usernameInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    connection.invoke("DisplayHistory").catch(function (messages) {
+        console.log(messages);
+        messages.forEach(message => {
+            var usernameText = document.createElement("b");
+            var messageText = document.createElement("div");
+            console.log(message);
+            usernameText.textContent = message.user + " - ";
+            messageText.textContent = message.text;
+            messageText.classList.add("alert");
+            messageText.classList.add("alert-success");
+            messageText.prepend(usernameText);
+
+            document.getElementById("messagesList").appendChild(messageText);
+        });
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+
