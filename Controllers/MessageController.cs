f305338df35c5ea6fa05d6be41b3fff6bf7ef9e6
Petru <martincu.petru@gmail.com>
2020-03-22 17:54:00 +0200

    Added controller for message (add and get all)

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LightChatApp.DataAccess;
+using LightChatApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LightChatApp.Controllers
+{
+    public static class MessageController
+    {
+        public static void AddMessage(Message message)
+        {
+            using (var context = new ChatContext())
+            {
+                message.Id = Guid.NewGuid().ToString();
+                context.Messages.Add(message);
+                context.SaveChanges();
+            }
+        }
+
+        public static DbSet<Message> GetMessages()
+        {
+            using (var context = new ChatContext())
+            {
+                return context.Messages;
+            }
+        }
+    }
+}
