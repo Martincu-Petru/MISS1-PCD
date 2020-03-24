b37071fcb277274f8fc0f5d951bb9092d2d9addd
Petru <martincu.petru@gmail.com>
2020-03-24 15:24:59 +0200

    Added helper functions to display history and translate

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,30 +1,19 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var receivedHistory = false;
 
 connection.on("ReceiveMessage", function (user, message) {
-    var encodedUsernameText = user + " - ";
-    var encodedMessageText = message;
-    var usernameText = document.createElement("b");
-    var messageText = document.createElement("div");
-
-    usernameText.textContent = encodedUsernameText;
-    messageText.textContent = encodedMessageText;
-    messageText.classList.add("alert");
-    messageText.classList.add("alert-success");
-    messageText.prepend(usernameText);
-
-    document.getElementById("messagesList").appendChild(messageText);
-
-});
-
-connection.on("ReceiveHistory", function (messages) {
-    messages.forEach(message => {
+    var userLanguage = navigator.language;
+    var googleApiLanguageCode = userLanguage.split('-')[0];
+    connection.invoke("TranslateText", message, googleApiLanguageCode).then(function (translatedMessage) {
+        var encodedUsernameText = user + " - ";
+        var encodedMessageText = translatedMessage;
         var usernameText = document.createElement("b");
         var messageText = document.createElement("div");
-        console.log(message);
-        usernameText.textContent = message.user + " - ";
-        messageText.textContent = message.text;
+
+        usernameText.textContent = encodedUsernameText;
+        messageText.textContent = encodedMessageText;
         messageText.classList.add("alert");
         messageText.classList.add("alert-success");
         messageText.prepend(usernameText);
@@ -33,24 +22,44 @@ connection.on("ReceiveHistory", function (messages) {
     });
 });
 
+connection.on("ReceiveHistory", function (messages) {
+    if (!receivedHistory) {
+        messages.forEach(message => {
+            var usernameText = document.createElement("b");
+            var messageText = document.createElement("div");
+
+            usernameText.textContent = message.user + " - ";
+            messageText.textContent = message.text;
+            messageText.classList.add("alert");
+            messageText.classList.add("alert-success");
+            messageText.prepend(usernameText);
+
+            document.getElementById("messagesList").appendChild(messageText);
+            receivedHistory = true;
+        });
+    }
+});
+
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("usernameInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
+        return console.error(err);
     });
     event.preventDefault();
 });
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
-    connection.invoke("DisplayHistory").catch(function (messages) {
-        console.log(messages);
+    var userLanguage = navigator.language;
+
+    var googleApiLanguageCode = userLanguage.split('-')[0] ;
+    connection.invoke("DisplayHistory", googleApiLanguageCode).then(function (messages) {
         messages.forEach(message => {
             var usernameText = document.createElement("b");
             var messageText = document.createElement("div");
-            console.log(message);
+
             usernameText.textContent = message.user + " - ";
             messageText.textContent = message.text;
             messageText.classList.add("alert");
@@ -61,7 +70,7 @@ connection.start().then(function () {
         });
     });
 }).catch(function (err) {
-    return console.error(err.toString());
+    return console.error(err);
 });
 
 
