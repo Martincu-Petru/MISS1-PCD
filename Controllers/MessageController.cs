738295b272a1434c32cbd8ed75305d6b6bfc9ebc
Petru <martincu.petru@gmail.com>
2020-03-24 15:23:55 +0200

    Calling Google Cloud function upon message send

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using LightChatApp.DataAccess;
 using LightChatApp.Models;
@@ -10,14 +11,20 @@ namespace LightChatApp.Controllers
 {
     public static class MessageController
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public static async Task AddMessage(Message message)
         {
             using (var context = new ChatContext())
             {
                 message.Id = Guid.NewGuid().ToString();
+                message.MessageDate = DateTime.Now;
                 context.Messages.Add(message);
                 await context.SaveChangesAsync();
             }
+
+            await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get,
+                new Uri("https://us-central1-light-chat-app.cloudfunctions.net/logging_requests?id=" + message.Id)));
         }
 
         public static async Task<List<Message>> GetMessages()
@@ -27,9 +34,8 @@ namespace LightChatApp.Controllers
             {
                 messages = await context.Messages.ToListAsync();
             }
-            messages.Reverse();
 
-            return messages;
+            return messages.OrderBy(x => x.MessageDate).ToList();
         }
     }
 }
