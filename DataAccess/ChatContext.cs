e859fcf2f3cde200a8c77f47b5beab74065d3e77
Petru <martincu.petru@gmail.com>
2020-03-24 15:24:13 +0200

    Changed cloud database connection string

--- DataAccess/ChatContext.cs
+++ DataAccess/ChatContext.cs
@@ -14,7 +14,9 @@ namespace LightChatApp.DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("Server=127.0.0.1;Port=3306;Database=chat_db;Uid=user;Pwd=user;");
+            optionsBuilder.UseMySQL("Uid=user;Pwd=user;Host=cloudsql;Database=chat_db;CharSet=utf8mb4;");
+            // optionsBuilder.UseMySQL("Server=light-chat-app:europe-west1:database;Database=chat_db;Uid=user;Pwd=user;CharSet=utf8mb4;");
+            // optionsBuilder.UseMySQL("Server=127.0.0.1;Database=chat_db;Uid=user;Pwd=user;CharSet=utf8mb4;");
         }
     }
 }
