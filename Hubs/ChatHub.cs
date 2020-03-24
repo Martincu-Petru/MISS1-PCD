068457347990fb8bebfc67fd0808b6445fb157fd
Petru <martincu.petru@gmail.com>
2020-03-24 15:22:46 +0200

    Added history for passed messages, translated by browser-language

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Google.Cloud.Language.V1;
 using LightChatApp.Controllers;
 using LightChatApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Google.Cloud.Translation.V2;
 
 namespace LightChatApp.Hubs
 {
@@ -13,6 +16,7 @@ namespace LightChatApp.Hubs
     {
         public async Task SendMessage(string user, string text)
         {
+
             if (user?.Length == 0)
             {
                 user = "Unknown";
@@ -22,12 +26,11 @@ namespace LightChatApp.Hubs
             {
                 text = "Nothing";
             }
-            MessageController.AddMessage(new Message(user, text));
 
             var client = LanguageServiceClient.Create();
             var response = client.AnalyzeSentiment(new Document()
             {
-                Content = text,
+                Content = TranslateText(text, "en"),
                 Type = Document.Types.Type.PlainText
             });
             var sentiment = response.DocumentSentiment;
@@ -45,7 +48,42 @@ namespace LightChatApp.Hubs
                 text += " 🤔";
             }
 
+            await MessageController.AddMessage(new Message(user, text));
             await Clients.All.SendAsync("ReceiveMessage", user, text);
+        }
+
+        public async Task DisplayHistory(string userLanguage)
+        {
+            var messages = await MessageController.GetMessages();
+            foreach (var message in messages)
+            {
+                message.Text = TranslateText(message.Text, userLanguage);
+            }
+            await Clients.All.SendAsync("ReceiveHistory", messages);
+        }
+
+        public string TranslateText(string text, string targetLanguage)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            TranslationClient client = TranslationClient.Create();
+            string sourceLanguage = DetectLanguage(text);
+            if (targetLanguage == sourceLanguage)
+            {
+                return text;
+            }
+            else
+            {
+                TranslationResult response = client.TranslateText(text, targetLanguage, sourceLanguage);
+                return response.TranslatedText;
+            }
+        }
+
+        public string DetectLanguage(string text)
+        {
+            TranslationClient client = TranslationClient.Create();
+            var detection = client.DetectLanguage(text);
+
+            return detection.Language;
         }
 
     }
