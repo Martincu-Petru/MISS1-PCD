c2969df892533472ad041edeca2aa76cdf2c34f4
Petru <martincu.petru@gmail.com>
2020-05-12 02:17:25 +0300

    Server saves books by isbn

--- cardinal-updater/myCardinalApp/node_server/app.js
+++ cardinal-updater/myCardinalApp/node_server/app.js
@@ -20,7 +20,25 @@ app.get("/api/book/:id", (req, res, next) => {
 });
 
 app.post("/api/book", (req, res, next) => {
-  console.log(req.body);
+
+  var data = {
+    bookName: req.body.bookName != null ? req.body.bookName : "null",
+    author: req.body.author != null ? req.body.author : "null",
+    //isbn: req.body.isbn != null ? req.body.isbn : "null",
+    isbn: Math.floor(Math.random() * Math.floor(10000000000)),
+    category: req.body.category != null ? req.body.category : "null",
+    publishingHouse: req.body.publishingHouse != null ? req.body.publishingHouse : "null",
+    text: req.body.text != null ? req.body.text : "null"
+  }
+
+  console.log(data);
+
+  fs.appendFile("./books/" + data.isbn + ".json", JSON.stringify(data), function (err) {
+    if (err) throw err;
+    console.log('Book was saved!');
+  });
+
+  console.log(JSON.stringify(data));
 });
 
 app.delete("/api/book", (req, res, next) => {
